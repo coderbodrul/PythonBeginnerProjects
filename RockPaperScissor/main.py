4040111a1b157dbd55d84b1a41c4df93d3d99021
coderbodrul <bodrulfeb@gmail.com>
2025-02-23 01:02:34 +0600

    after shortening the code

--- RockPaperScissor/main.py
+++ RockPaperScissor/main.py
@@ -1,56 +1,31 @@
 import random
 
 emoji_dict = {
-    "rock": "✊",
-    "paper": "✋",
-    "scissors": "✌"
+    "r": "✊",
+    "p": "✋",
+    "s": "✌"
 }
 
 rps = ['r', 'p', 's']
 
 while True:
-    com = random.randint(0, 2)
-    com = rps[com]
+    com = random.choice(rps)
     human = input("Rock, Paper or Scissor? (r/p/s): ").lower()
-
     if human not in rps:
         print('Invalid Choice')
     else:
-        if human == 'r':
-            print(f"Your Choice {emoji_dict['rock']}")
-            if com == 'r':
-                print(f"Computer Choice {emoji_dict['rock']}")
-                print("Tie!!!")
-            elif com == 'p':
-                print(f"Computer Choice {emoji_dict['paper']}")
-                print("You Won!!!")
-            else:
-                print(f"Computer Choice {emoji_dict['scissors']}")
-                print("You Lost!!!")
-            
-        elif human == 'p':
-            print(f"Your Choice {emoji_dict['paper']}")
-            if com == 'r':
-                print(f"Computer Choice {emoji_dict['rock']}")
-                print("You Won!!!")
-            elif com == 'p':
-                print(f"Computer Choice {emoji_dict['paper']}")
-                print("Tie!!!")
-            else:
-                print(f"Computer Choice {emoji_dict['scissors']}")
-                print("You Lost!!!")
+        print(f"You chose {emoji_dict[human]}")
+        print(f"Computer chose {emoji_dict[com]}")
         
+        if com == human:
+            print("Tie!!")
+        elif (
+            (human == 'r' and com == 's') or
+            (human == 'p' and com == 'r') or
+            (human == 's' and com == 'p')):
+            print("You Won!!!")
         else:
-            print(f"Your Choice {emoji_dict['scissors']}")
-            if com == 'r':
-                print(f"Computer Choice {emoji_dict['rock']}")
-                print("You Lost!!!")
-            elif com == 'p':
-                print(f"Computer Choice {emoji_dict['paper']}")
-                print("You Won!!!")
-            else:
-                print(f"Computer Choice {emoji_dict['scissors']}")
-                print("Tie!!!")
+            print("You Lost!!!")
         prompt = input("Continue? (y/n): ").lower()
         if prompt == 'n':
             break
